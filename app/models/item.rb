073b0666ee79d2baf5ddcb829ea226198df23e2f
RRS1107 <thh0508@gmail.com>
2022-07-16 13:54:24 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,8 +9,9 @@ class Item < ApplicationRecord
   validates :postage_type_id, numericality: { other_than: 1 , message: " can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: " can't be blank"}
   validates :preparation_day_id, numericality: { other_than: 1 , message: " can't be blank"}
-  validates :price,numericality: { in: 300..9_999_999, message: " is out of setting range" }
-
+  validates :price, numericality: { in: 300..9_999_999, message: " is out of setting range" }
+  validates :price, numericality: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters" }
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :prefecture
