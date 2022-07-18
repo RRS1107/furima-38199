d5972bfdd98b04cd575d9351de5a8b8ca73e1ba9
RRS1107 <thh0508@gmail.com>
2022-07-18 18:38:04 +0900

    削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only:[:new, :edit]
   before_action :prevent_url, only: [:edit, :update, :destroy]
-  before_action :find_params, only:[:show, :edit, :update,]
+  before_action :find_params, only:[:show, :edit, :update, :destroy]
 
   def index
     @item = Item.all.order("created_at DESC")
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
   
   private
